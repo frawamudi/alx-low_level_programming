c5f90d17e2d5bbc160804aee71226dbce93de836
frawamudi <frawamudi@gmail.com>
2022-06-02 11:20:08 -0700

    put something out on the stdio using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * main: This c program outputs string Programming is like
+ * main: Entry point
+ * Description: This c program outputs string Programming is like
  * building a multilingual puzzle.
  * Return: 0 if exited properly, non-zero otherwise
  */
 
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
-return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
