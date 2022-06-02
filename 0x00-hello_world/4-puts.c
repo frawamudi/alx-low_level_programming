adfcc79c648717bf6798d1331d0af2c1abeb28b1
frawamudi <frawamudi@gmail.com>
2022-06-02 11:05:14 -0700

    put something out on the stdio using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * This c program outputs string "Programming is like
+ * building a multilingual puzzle".
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+}
