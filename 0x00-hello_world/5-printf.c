569bf3eb398fd3d346efef276d229238631166d6
frawamudi <frawamudi@gmail.com>
2022-06-02 11:59:58 -0700

    printing to stdio using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print a string with printf()
+ * Description: printing a string
+ * Return: 0
+ */
+
+int main()
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+
+}
