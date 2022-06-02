118fcd48f0f5b338c1b7c18515650ddd14491c43
frawamudi <frawamudi@gmail.com>
2022-06-02 12:03:25 -0700

    printing to stdio using printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,9 +6,9 @@
  * Return: 0
  */
 
-int main()
+int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 
 }
