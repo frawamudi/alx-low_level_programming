eb3eb63d8c7bf8a93dda40bb8556a9cfdb8351cb
frawamudi <frawamudi@gmail.com>
2022-06-07 03:53:54 -0700

    first push with main.h and _putchar()

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  * main -  prints Holberton, followed by a new line
+ *   * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char my_string[] = "_putchar";
+
+	while( *my_string != '\0')
+	{
+		_putchar(*my_string);
+		my_string++;
+	}
+
+	return(0);
+}
