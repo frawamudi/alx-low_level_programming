a016679b6f0e7d30f06ef6a2f3ea1f9e62dff714
frawamudi <frawamudi@gmail.com>
2022-06-09 11:45:00 -0700

    drawing a diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,24 +1,27 @@
 #include "main.h"
 
 /**
- * print_diagonal - diagonal lines made of backslashes
- * @n: number of \ to be printed
- * Return: 0
+ * print_diagonal - Print diagonal line dependent on the integer n.
+ * @n : The number of lines using '\' characters to use
+ * Return: Void.
  */
-
 void print_diagonal(int n)
 {
-	int a, b;
+	int i;
+	int spaces;
+
+	for (i = 0; i < n; i++)
+	{
+		for (spaces = 0; spaces < i; spaces++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
 
 	if (n <= 0)
-		 _putchar('\n');
-	for (a = 0; a < n; a++)
 	{
-		for (b = 0; b < a; b++)
-			{
-				_putchar(' ');
-			}
-		_putchar('\\');
 		_putchar('\n');
 	}
 }
