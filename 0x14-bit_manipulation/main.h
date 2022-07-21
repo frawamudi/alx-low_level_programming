b279747a2d4d132465fb1254cc985eb830fe53dd
frawamudi <frawamudi@gmail.com>
2022-07-21 13:21:03 -0700

    print binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,9 +4,6 @@
 #include <stddef.h>
 #include <stdio.h>
 
-#ifdef DEBUG /*prints debug statements*/
-#endif
-
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
